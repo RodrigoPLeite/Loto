48a40ca64ab056a1f5f6a5b101c3ab50be5da871
Rodrigo Leite <rodrigopleite@yahoo.com.br>
2021-09-05 10:47:24 -0300

    Fazendo o For

--- src/Sorteio.java
+++ src/Sorteio.java
@@ -1,6 +1,16 @@
-
 public class Sorteio {
     public Sorteio() {
 
+    }
+
+    public void numerosSorteados() {
+        int[] numeros = new int[25];
+        for (int i = 0; i < 25; i++) {
+            numeros[i] = i + 1;
+        }
+
+        for (int numero : numeros) {
+            System.out.println(numero);
+        }
     }
 }
