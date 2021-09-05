ff03dc4a429665411036b3e4708ac35301a5dc67
Rodrigo Leite <rodrigopleite@yahoo.com.br>
2021-09-05 11:30:21 -0300

    Méetodo de sorteio adicionado

--- src/Sorteio.java
+++ src/Sorteio.java
@@ -1,16 +1,26 @@
-public class Sorteio {
-    public Sorteio() {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Sorteio {
+
+    public List<Integer> numerosDisponiveis() {
+        int[] numeros = new int[25];
+        List<Integer> lista = new ArrayList<>();
+        for (int i = 0; i < 25; i++) {
+            numeros[i] = i + 1;
+            lista.add(numeros[i]);
+        }
+        return lista;
     }
 
     public void numerosSorteados() {
-        int[] numeros = new int[25];
-        for (int i = 0; i < 25; i++) {
-            numeros[i] = i + 1;
-        }
-
-        for (int numero : numeros) {
-            System.out.println(numero);
+        int contador = 0;
+        List<Integer> lista = numerosDisponiveis();
+        Collections.shuffle(lista);
+        while (contador < 15) {
+            System.out.print(lista.get(contador) + " / ");
+            contador++;
         }
     }
 }
